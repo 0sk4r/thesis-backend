ef524deeacadcdf8624a7cc3bb15d8b0c9f648ba
Oskar Sobczyk <sobczykoskar@gmail.com>
2019-05-06 10:55:22 +0200

    Add token authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :test
+    get '/test2', to: 'test#test'
+  end
+
 end
