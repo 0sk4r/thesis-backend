a337cb6b492cb234a140a48cb634c61cda595fd8
Oskar Sobczyk <sobczykoskar@gmail.com>
2019-05-08 20:07:17 +0200

    Handle error status in posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,28 +1,27 @@
 # frozen_string_literal: true
 
 module Api
-    class PostsController < ApplicationController
-        before_action :authenticate_user!, only: [:create]
+  class PostsController < ApplicationController
+    before_action :authenticate_user!, only: [:create]
 
-      def index
-        render json: { "test": 'testowa' }
-      end
-  
+    def index
+      render json: { "test": 'testowa' }
+    end
+
     def create
-        post = Post.new(post_params)
+      post = Post.new(post_params)
 
-        respond = if post.save
-            {"message": "Succes"}
-          else
-            {"error": post.errors.full_messages.join('. ')}
-          end
-
-        render json: respond
+      if post.save
+        render json: { "message": 'Post successfully created', "post": post }
+      else
+        render status: 422, json: { "errors": post.errors.full_messages.join('. ') }
+      end
     end
 
     private
+
     def post_params
-        params.require(:post).permit(:content, :title, :user_id)
+      params.permit(:content, :title).merge(user_id: current_user.id)
     end
   end
 end
