31fc64a57c20dc58a921603e2738cd3f82124895
Oskar <sobczykoskar@gmail.com>
2019-07-23 16:53:59 +0200

    user edit

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,9 @@
 
 module Api
   class UsersController < ApplicationController
+
+    before_action :authenticate_user!, only: [:update, :edit]
+
     def find
       provider = UserProvider.new(params[:key])
       render json: provider.results
@@ -16,6 +19,30 @@ module Api
     def user_posts
       @user = User.find(params[:id])
       render json: @user.posts
+    end
+
+    def edit
+      @user = User.find(current_user.id)
+
+      render json: @user
+    end
+
+    def update
+      @user = User.find(current_user.id)
+
+      @user.update(user_params)
+
+      if @user.save
+        render json: { "message": 'User successfully updated', "user": @user }
+      else
+        render status: 422, json: { "errors": @user.errors.full_messages.join('. ') }
+      end
+    end 
+
+    private
+
+    def user_params
+      params.permit(:name, :nickname, :image)
     end
   end
 end
