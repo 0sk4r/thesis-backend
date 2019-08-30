7aa73e54a01f5ba2e7d3d6317e2c8339898271df
Oskar <sobczykoskar@gmail.com>
2019-08-31 00:38:15 +0200

    final changes

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -5,4 +5,6 @@ class Follow < ApplicationRecord
                                      message: 'You are following this user' }
   belongs_to :follower, class_name: 'User', foreign_key: 'follower_id'
   belongs_to :following, class_name: 'User', foreign_key: 'following_id'
+
+  has_one :notification, as: :action
 end
